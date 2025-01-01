c337d026248eb696e78e38bb387d0f5e32dc4084
byteZorvin <imehul24@gmail.com>
2025-01-01 15:16:36 +0530

    sample nest + prisma + postgres + testcontainer

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,90 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { INestApplication } from '@nestjs/common';
+// import * as request from 'supertest';
+// import { AppModule } from './../src/app.module';
+//
+// describe('AppController (e2e)', () => {
+//   let app: INestApplication;
+//
+//   beforeEach(async () => {
+//     const moduleFixture: TestingModule = await Test.createTestingModule({
+//       imports: [AppModule],
+//     }).compile();
+//
+//     app = moduleFixture.createNestApplication();
+//     await app.init();
+//   });
+//
+//   it('/ (GET)', () => {
+//     return request(app.getHttpServer())
+//       .get('/')
+//       .expect(200)
+//       .expect('Hello World!');
+//   });
+// });
+//
+//
+
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from "@testcontainers/postgresql";
+import { PrismaClient } from "@prisma/client";
+import { Test, TestingModule } from "@nestjs/testing";
+
+import { AppModule } from "../src/app.module";
+import { INestApplication } from "@nestjs/common";
+
+import { execSync } from "child_process";
+
+
+let prismaClient: PrismaClient;
+let pgContainer: StartedPostgreSqlContainer;
+let app: INestApplication;
+
+describe("AppController (e2e)", () => {
+  beforeAll(async () => {
+    pgContainer = await new PostgreSqlContainer()
+      // .withDatabase("test_db")
+      // .withUser("test_user")
+      // .withPassword("pass")
+      .start();
+    const connectionURI = pgContainer.getConnectionUri();
+    console.log("PostgreSQL container started with uri: ", connectionURI);
+
+    prismaClient = new PrismaClient({
+      datasourceUrl: connectionURI,
+      log: ["query"],
+    });
+
+    prismaClient.$connect();
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    execSync("npm",
+      {
+        env: { DATABASE_URL: connectionURI },
+      });
+
+    // In beforeAll:
+    // await exec(`db push`, {
+    //   env: { DATABASE_URL: connectionURI }
+    // });
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+  it("should have created all required tables", async () => {
+    const tables: any = await prismaClient.$queryRaw`
+      SELECT table_name
+      FROM information_schema.tables 
+      WHERE table_schema = 'public'
+    `;
+    console.log("Tables: ", tables);
+
+    const tableNames = tables.map((t) => t.table_name);
+    expect(tableNames).toContain("User");
+  });
+});
